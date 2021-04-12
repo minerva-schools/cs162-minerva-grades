aa7d66523f44e6f2c39d650b89e432205b3469e6
pelegshilo <peleg@minerva.kgi.edu>
2021-04-12 17:59:24 +0900

    Update project to use environment variables. Fixed dependency issues

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'de26a2ccabac416b0cc068d4051cee04'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
--- unit_test/GradeFetcherTest.py
+++ unit_test/GradeFetcherTest.py
@@ -3,7 +3,9 @@ import unittest
 from dashboard import db
 from dashboard.models import Lo, LoGrade, Hc, HcGrade
 from dashboard.GradeFetcher import LoFetcher, HcFetcher
+from dotenv import load_dotenv
 
+load_dotenv()
 # To run this, set the environment variable SESSION_ID to your Forum session id
 session_id = os.environ.get("SESSION_ID")
 class FetcherTest(unittest.TestCase):
