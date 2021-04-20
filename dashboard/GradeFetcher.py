47b276f6f374ee528fc5d351ceed0f11463ed3ac
pelegshilo <peleg@minerva.kgi.edu>
2021-04-20 18:10:55 +0900

    - Added Regex verification to LOs - Added functions to calculate transfers - Added a test for transfer calculation

--- dashboard/GradeFetcher.py
+++ dashboard/GradeFetcher.py
@@ -3,10 +3,12 @@ from dashboard.models import Lo, LoGrade, Hc, HcGrade
 from dashboard import db
 import dateutil.parser
 import concurrent.futures
+import re
 
 
 class GradeFetcher():
     """Parent class that fetches grades from Forum"""
+
     def __init__(self, session):
         self._cookie = dict(sessionid=session)
         self.api_base = "https://forum.minerva.kgi.edu/api/v1/"
@@ -67,8 +69,8 @@ class HcFetcher(GradeFetcher):
             time = dateutil.parser.parse(grade["created-on"])
             transferred = self.is_transferred(grade["hc-item"], grade)
             grades.append(HcGrade(grade_id=grade["id"], hc_id=hc_id, user_id=self._cookie["sessionid"],
-                                score=grade["score"], weight=grade["weight"], time=time,
-                                assignment=(grade["type"] == "assignment"), transfer=transferred))
+                                  score=grade["score"], weight=grade["weight"], time=time,
+                                  assignment=(grade["type"] == "assignment"), transfer=transferred))
         return grades
 
     def is_transferred(self, hc_id, grade):
@@ -98,6 +100,7 @@ class LoFetcher(GradeFetcher):
         terms_list = self.get_url("terms/with-lo-trees")
         terms = [term["id"] for term in terms_list]
         urls = []
+        valid_courses = re.compile('((CS|NS|B|AH|SS|CP)1)|IL181')
         for term in terms:
             # Get mean LO grades
             lo_means_raw = self.get_url("outcome-index-items?termId={}&outcomeType=lo".format(str(term)))
@@ -110,12 +113,14 @@ class LoFetcher(GradeFetcher):
             # fetches information for every LO in the term
             for course in lo_tree:
                 for co in course["course-objectives"]:
-                    for lo in co["learning-outcomes"]:
-                        urls.append(lo["id"])
-                        new_lo = Lo(lo_id=lo["id"], user_id=self._cookie["sessionid"], name=lo["name"],
-                                    description=lo["description"], term=term, co_id=co["id"], co_desc=co["description"],
-                                    course=course["course"]["course-code"], mean=lo_means[lo["id"]])
-                        db.session.add(new_lo)
+                    if valid_courses.match(course["course"]["course-code"]) is not None:
+                        for lo in co["learning-outcomes"]:
+                            urls.append(lo["id"])
+                            new_lo = Lo(lo_id=lo["id"], user_id=self._cookie["sessionid"], name=lo["name"],
+                                        description=lo["description"], term=term, co_id=co["id"],
+                                        co_desc=co["description"],
+                                        course=course["course"]["course-code"], mean=lo_means[lo["id"]])
+                            db.session.add(new_lo)
         self.db_commit()
         # Fetches individual LO grades
         with concurrent.futures.ThreadPoolExecutor(max_workers=20) as pool:
@@ -123,6 +128,7 @@ class LoFetcher(GradeFetcher):
                 if grades is not None:
                     db.session.add_all(grades)
         self.db_commit()
+
     def get_grade(self, lo_id):
         """Checks whether or not a grade is transferred by looking at its corresponding 'focused outcomes'"""
         grades_data = self.get_url("outcomeindex/performance?learning-outcome=" + str(lo_id))
@@ -130,6 +136,6 @@ class LoFetcher(GradeFetcher):
         for grade in grades_data:
             time = dateutil.parser.parse(grade["created-on"])
             grades.append(LoGrade(grade_id=grade["id"], lo_id=lo_id, user_id=self._cookie["sessionid"],
-                                score=grade["score"], weight=grade["weight"], time=time,
-                                assignment=(grade["type"] == "assignment")))
+                                  score=grade["score"], weight=grade["weight"], time=time,
+                                  assignment=(grade["type"] == "assignment")))
         return grades
--- dashboard/grade_calculations.py
+++ dashboard/grade_calculations.py
@@ -1,10 +1,56 @@
 from dashboard.GradeFetcher import GradeFetcher, HcFetcher, LoFetcher
 from dashboard import db
-from dashboard.models import Hc, HcGrade
+from dashboard.models import Hc, HcGrade, Lo
 import pandas as pd
+import math
 
-def get_transfers():
-    hcs = pd.read_sql(db.session.query(Hc).statement, db.session.bind)
-    grades = pd.read_sql(db.session.query(HcGrade).statement, db.session.bind)
-    for hc in hcs:
-        grades 
+
+def get_transfers(user_id, course, end_date=None):
+    """For a certain course and timeframe, gives a dataframe with the HCs for that course,
+     augmented with their transfer information, including transfer weight, transfer score, and 0 or 1 depending on
+     whether or not the HC was transferred"""
+    if end_date:
+        hcs = pd.read_sql(db.session.query(Hc).filter_by(user_id=user_id).filter_by(course=course).statement,
+                          db.session.bind)
+        grades = pd.read_sql(db.session.query(HcGrade).filter_by(user_id=user_id).filter(HcGrade.time <= course)
+                             .statement, db.session.bind)
+    else:
+        hcs = pd.read_sql(db.session.query(Hc).filter_by(user_id=user_id).filter_by(course=course).statement,
+                      db.session.bind)
+        grades = pd.read_sql(db.session.query(HcGrade).filter_by(user_id=user_id).statement, db.session.bind)
+    # I haven't been able to nail the exact calculation here, but this should do a decent enough job at it
+    courses_taken = 9 + db.session.query(Lo.course).distinct().count()
+    hcs["transfer_weight"] = 0
+    hcs["transfer_score"] = float(0)
+    hcs["transferred"] = 0
+    hcs["mean"] = hcs["mean"].astype(float)
+    for hc in hcs.itertuples():
+        transfer_grades = grades.query('hc_id == {} & transfer'.format(hc.hc_id))
+        transfer_weight = transfer_grades[transfer_grades.score >= 3].weight.sum() - \
+                          transfer_grades[transfer_grades.score < 3].weight.sum()
+        transfer_score = calc_transfer(transfer_weight, courses_taken)
+        hcs.at[hc.Index, "transfer_weight"] = transfer_weight
+        hcs.at[hc.Index, "transfer_score"] = transfer_score
+        hcs.at[hc.Index, "transferred"] = 1 if transfer_score >= 3 else 0
+    return hcs
+
+
+def calc_transfer(weight, courses_taken):
+    """Calculates the transfer score for an HC """
+    # a bunch of constants that Minerva uses
+    alpha = 0.385
+    mu = 0.2
+    v = 1.818
+    return 1 + (4 / (1 + math.e ** (-alpha * (weight - (mu * courses_taken)))) ** v)
+
+
+def calc_cornerstone_score(user_id, course, time):
+    """Calculates the Minerva score (1-5) for a cornerstone at a certain time, including transfers"""
+    hcs = get_transfers(user_id, course, time)
+    mastery = hcs["mean"].mean()
+    transfer_competence = hcs["transfer_score"].mean()
+    transferred_percent = hcs["transferred"].mean()
+    transfer_scope = min(1 + 2.35 * (transferred_percent / 0.4), 5)
+    print(mastery, transfer_competence, transfer_scope)
+    score = 0.6 * mastery + 0.25 * transfer_scope + 0.15 * transfer_competence
+    return round(score, 2)
